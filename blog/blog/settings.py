7b96588c8b5e93ba9356f90106fffe888100e8cc
Pan BeiBei <1132432040@qq.com>
2023-07-18 20:11:45 +0800

    添加redis缓存，并将session存储由数据库改为redis

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -88,6 +88,27 @@ DATABASES = {
     }
 }
 
+#redis的配置
+CACHES = {
+    "default": {
+        "BACKEND":"django_redis.cache.RedisCache",
+        "LOCATION":"redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS":"django_redis.client.DefaultClient",
+        }
+    },
+    "session": {
+        "BACKEND":"django_redis.cache.RedisCache",
+        "LOCATION":"redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS":"django_redis.client.DefaultClient",
+        }
+    }
+}
+#session由数据库存储改为redis
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
